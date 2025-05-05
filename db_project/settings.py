f896370bd95362d52235c1cf90febbf1d4e9758a
Khoa Tran <khoatrandd.139@gmail.com>
2025-05-05 19:01:37 +0700

    chore: add sql query logs

--- db_project/settings.py
+++ db_project/settings.py
@@ -143,3 +143,19 @@ LOGIN_REDIRECT_URL = '/auth/dashboard/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
